603015ebe6c76f934205f8d4faff7559b5919234
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-01 13:09:59 +0530

    Implimented hero section animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
-
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <div className="h-dvh border border-red-500" />
     </main>
   );
 };
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,4 +1,50 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const HeroSection = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", {
+      type: "chars",
+    });
+    const tl = gsap.timeline({
+      delay: 1,
+    });
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(titleSplit.chars, {
+        yPercent: 100,
+        stagger: 0.02,
+        ease: "power2.out",
+      });
+
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    heroTl.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
+  });
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
@@ -7,13 +53,13 @@ const HeroSection = () => {
           alt="Hero Image"
           className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
         />
-        <div className="hero-content">
+        <div className="hero-content opacity-0">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freeking Delicious</h1>
           </div>
           <div
             style={{
-              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+              clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)",
             }}
             className="hero-text-scroll"
           >
