a6e57711461f0e60cb87ff348462bace783a6cf1
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-02 14:05:48 +0530

    Added the flavortitle animation

--- src/components/FlvourTitle.jsx
+++ src/components/FlvourTitle.jsx
@@ -1,10 +1,58 @@
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
 const FlvourTitle = () => {
+  useGSAP(() => {
+    const firstTextSplit = SplitText.create(".first-text-split h1", {
+      type: "chars",
+    });
+    const secondTextSplit = SplitText.create(".second-text-split h1", {
+      type: "chars",
+    });
+
+    gsap.from(firstTextSplit.chars, {
+      yPercent: 200,
+      stagger: 0.02,
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "top 30%",
+        markers: true,
+      },
+    });
+    gsap.to(".flavor-text-scroll", {
+      duration: 1,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "top 10%",
+        markers: true,
+      },
+    });
+
+    gsap.from(secondTextSplit.chars, {
+      yPercent: 200,
+      stagger: 0.02,
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "top 1%",
+        markers: true,
+      },
+    });
+  });
   return (
     <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16">
       <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
         <h1>We have 6</h1>
       </div>
-      <div style={{}} className="flavor-text-scroll">
+      <div
+        style={{
+          clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
+        }}
+        className="flavor-text-scroll"
+      >
         <div className="bg-mid-brown pb-5 2xl:pt-0 pt-3 2xl:px-5 px-3">
           <h2 className="text-milk">freaking</h2>
         </div>
